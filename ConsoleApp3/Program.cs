6a8789e3953710d57e148943c15f063aacc0e537
nihatab104 <146129654+nihatab104@users.noreply.github.com>
2023-10-10 00:08:49 +0400

    first commit

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static string DuzgunFormat(string cumle)
+    {
+        StringBuilder duzgunCumle = new StringBuilder();
+        bool kelimeBaslangici = true;
+
+        foreach (char karakter in cumle)
+        {
+            if (char.IsWhiteSpace(karakter))
+            {
+          
+                kelimeBaslangici = true;
+                continue;
+            }
+
+            if (kelimeBaslangici)
+            {
+                
+                duzgunCumle.Append(char.ToUpper(karakter));
+                kelimeBaslangici = false;
+            }
+            else
+            {
+   
+                duzgunCumle.Append(char.ToLower(karakter));
+            }
+        }
+
+        return duzgunCumle.ToString();
+    }
+
+    static void Main()
+    {
+        string inputCumle = "        EverytHing  i neeD,    is a CuP   of       CoFFee    ";
+        string outputCumle = DuzgunFormat(inputCumle);
+
+        Console.WriteLine(outputCumle);
+    }
+}
